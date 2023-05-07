d2c535633e91469284a1d7860a9eb8ecc5d4a055
Luc Vo <luke.syvo@gmail.com>
2023-05-07 14:32:57 -0600

    .

--- src/components/ProductFilter.js
+++ src/components/ProductFilter.js
@@ -6,22 +6,22 @@ export const SORT_BY_OPTIONS = [
   { value: "featured", label: "Featured" },
   { value: "newest", label: "Newest" },
   { value: "priceDesc", label: "Price: High-Low" },
-  { value: "priceAsc", label: "Price: Low-High" },
+  { value: "priceAsc", label: "Price: Low-High" }
 ];
 
 export const FILTER_GENDER_OPTIONS = ["Men", "Women", "Kids"];
 
 export const FILTER_CATEGORY_OPTIONS = [
   "All",
-  "Shose",
+  "Shoes",
   "Apparel",
-  "Accessories",
+  "Accessories"
 ];
 
 export const FILTER_PRICE_OPTIONS = [
   { value: "below", label: "Below $25" },
   { value: "between", label: "Between $25 - $75" },
-  { value: "above", label: "Above $75" },
+  { value: "above", label: "Above $75" }
 ];
 
 function ProductFilter({ resetFilter }) {
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -102,7 +102,7 @@ function applyFilter(products, filters) {
   }
 
   // FILTER PRODUCTS
-  if (filters.gender) {
+  if (filters.gender.length) {
     filteredProducts = products.filter((product) =>
       filters.gender.includes(product.gender)
     );
