5049979c96d74a123fa079a2bc4f17f073f505fd
Luc Vo <luke.syvo@gmail.com>
2023-08-01 10:49:41 -0600

    .

--- src/pages/admin/OrdersPage.js
+++ src/pages/admin/OrdersPage.js
@@ -1,7 +1,35 @@
+import {
+  Container,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow
+} from "@mui/material";
 import React from "react";
 
 function OrdersPage() {
-  return <div>OrdersPage</div>;
+  return (
+    <Container>
+      <TableContainer>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Order</TableCell>
+              <TableCell>User</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            <TableRow>
+              <TableCell></TableCell>
+              <TableCell></TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Container>
+  );
 }
 
 export default OrdersPage;
--- src/pages/admin/UsersPage.js
+++ src/pages/admin/UsersPage.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from "react";
+import {
+  Container,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow
+} from "@mui/material";
 
 function UsersPage() {
   return (
-    <div>UsersPage</div>
-  )
+    <Container>
+      <TableContainer>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>User</TableCell>
+              <TableCell>Info</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            <TableRow>
+              <TableCell></TableCell>
+              <TableCell></TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Container>
+  );
 }
 
-export default UsersPage
+export default UsersPage;
