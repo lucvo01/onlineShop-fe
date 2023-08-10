914aca9974ba2aa38dee721d547a4a8082dc6619
lucvo01 <luke.syvo@gmail.com>
2023-08-10 20:12:52 +0000

    .

---
+++ src/pages/HomePage1.js
@@ -0,0 +1,104 @@
+import React, { useState, useEffect } from "react";
+import { Img, Box, Container, Stack } from "@mui/material";
+import ProductFilter from "../components/product/ProductFilter";
+import ProductSearch from "../components/product/ProductSearch";
+import ProductSort from "../components/product/ProductSort";
+import ProductList from "../components/product/ProductList";
+import { FormProvider } from "../components/form";
+import { useForm } from "react-hook-form";
+import orderBy from "lodash/orderBy";
+import LoadingScreen from "../components/LoadingScreen";
+import PaginationBar from "../components/PaginationBar";
+import { useDispatch, useSelector } from "react-redux";
+import { getProducts } from "../components/slices/productsSlice";
+import { Grid } from "@mui/material";
+// import SearchInput from "../components/SearchInput";
+
+function HomePage1() {
+  const dispatch = useDispatch();
+  const [pageNum, setPageNum] = useState(1);
+
+  const { products, isLoading, totalPages, error } = useSelector(
+    (state) => state.products
+  );
+
+  const defaultValues = {
+    gender: [],
+    category: "",
+    priceRange: "",
+    sortBy: "featured",
+    searchQuery: ""
+  };
+  const methods = useForm({
+    defaultValues
+  });
+  const { watch, reset } = methods;
+  const filters = watch();
+  const filterProducts = applyFilter(products, filters);
+
+  const { searchQuery, gender, category } = filters;
+
+  useEffect(() => {
+    dispatch(getProducts({ pageNum, searchQuery, gender, category }));
+  }, [dispatch, pageNum, searchQuery, gender, category]);
+
+  return (
+    <Container>
+      <Grid container>
+        <Img src="https://content.asos-media.com/-/media/homepages/unisex/generic-hp/july-2023/julymonthly_12062023_womens_shot07_034_1440x698.jpg" />
+      </Grid>
+    </Container>
+  );
+}
+export default HomePage1;
+
+function applyFilter(products, filters) {
+  const { sortBy, gender, category, priceRange, searchQuery } = filters;
+
+  let filteredProducts = [...products];
+
+  // SORT BY
+  if (sortBy === "featured") {
+    filteredProducts = orderBy(filteredProducts, ["sold"], ["desc"]);
+  } else if (sortBy === "newest") {
+    filteredProducts = orderBy(filteredProducts, ["createdAt"], ["desc"]);
+  } else if (sortBy === "priceDesc") {
+    filteredProducts = orderBy(filteredProducts, ["price"], ["desc"]);
+  } else if (sortBy === "priceAsc") {
+    filteredProducts = orderBy(filteredProducts, ["price"], ["asc"]);
+  }
+
+  // FILTER PRODUCTS
+  // if (gender && gender.length > 0) {
+  //   filteredProducts = filteredProducts.filter((product) =>
+  //     gender.includes(product.gender)
+  //   );
+  // }
+
+  // if (category !== "All") {
+  //   filteredProducts = filteredProducts.filter(
+  //     (product) => product.category === category
+  //   );
+  // }
+
+  // if (priceRange) {
+  //   filteredProducts = filteredProducts.filter((product) => {
+  //     if (priceRange === "below") {
+  //       return product.price < 25;
+  //     } else if (priceRange === "between") {
+  //       return product.price >= 25 && product.price <= 75;
+  //     } else if (priceRange === "above") {
+  //       return product.price > 75;
+  //     }
+  //     return true;
+  //   });
+  // }
+
+  // if (searchQuery) {
+  //   filteredProducts = filteredProducts.filter((product) =>
+  //     product.name.toLowerCase().includes(searchQuery.toLowerCase())
+  //   );
+  // }
+
+  return filteredProducts;
+}
